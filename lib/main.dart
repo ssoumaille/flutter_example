e8dad1038036eb7d3526b71d4fbcb4126918cfef
Sacha Soumaille <sacha.soumaille-ext@softilys.com>
2022-10-05 14:28:25 +0200

    Drawer + Bottom Bar

--- lib/main.dart
+++ lib/main.dart
@@ -11,58 +11,205 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: appTitle,
-      home: MyHomePage(title: appTitle),
-    );
-  }
+  Widget build(BuildContext context) => const MaterialApp(
+        title: appTitle,
+        home: MyHomePage(title: appTitle),
+      );
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: const Center(
-        child: Text('My page'),
-      ),
-      drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<MyHomePage> {
+  int pageIndex = 0;
+
+  final pages = [
+    const Page1(),
+    const Page2(),
+    const Page3(),
+    const Page4(),
+  ];
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: Text(MyApp.appTitle)),
+        body: pages[pageIndex],
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Drawer Header'),
               ),
-              child: Text('Drawer Header'),
-            ),
-            ListTile(
-              title: const Text('Item 1'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-          ],
+              ListTile(
+                title: const Text('Item 1'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                  Navigator.canPop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Item 2'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+            ],
+          ),
         ),
-      ),
-    );
-  }
+        bottomNavigationBar: Container(
+          height: 60,
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {
+                  setState(() {
+                    pageIndex = 0;
+                  });
+                },
+                icon: const Icon(
+                  Icons.home_outlined,
+                  color: Colors.white,
+                  size: 35,
+                ),
+              ),
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {
+                  setState(() {
+                    pageIndex = 1;
+                  });
+                },
+                icon: const Icon(
+                  Icons.work_outline_outlined,
+                  color: Colors.white,
+                  size: 35,
+                ),
+              ),
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {
+                  setState(() {
+                    pageIndex = 2;
+                  });
+                },
+                icon: const Icon(
+                  Icons.widgets_outlined,
+                  color: Colors.white,
+                  size: 35,
+                ),
+              ),
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {
+                  setState(() {
+                    pageIndex = 3;
+                  });
+                },
+                icon: const Icon(
+                  Icons.person_outline,
+                  color: Colors.white,
+                  size: 35,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Container(
+        color: const Color(0xffC4DFCB),
+        child: Center(
+          child: Text(
+            "Page Number 1",
+            style: TextStyle(
+              color: Colors.green[900],
+              fontSize: 45,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      );
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Container(
+        color: const Color(0xffC4DFCB),
+        child: Center(
+          child: Text(
+            "Page Number 2",
+            style: TextStyle(
+              color: Colors.green[900],
+              fontSize: 45,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      );
+}
+
+class Page3 extends StatelessWidget {
+  const Page3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Container(
+        color: const Color(0xffC4DFCB),
+        child: Center(
+          child: Text(
+            "Page Number 3",
+            style: TextStyle(
+              color: Colors.green[900],
+              fontSize: 45,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      );
+}
+
+class Page4 extends StatelessWidget {
+  const Page4({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Container(
+        color: const Color(0xffC4DFCB),
+        child: Center(
+          child: Text(
+            "Page Number 4",
+            style: TextStyle(
+              color: Colors.green[900],
+              fontSize: 45,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      );
 }
