6cb4cc2b8061dbd9757248e7bd720b60fed4920e
Sacha Soumaille <sacha.soumaille-ext@softilys.com>
2022-10-06 11:52:01 +0200

    PageView, Navigator

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,17 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) => const MaterialApp(
+  Widget build(BuildContext context) => MaterialApp(
         title: appTitle,
-        home: MyHomePage(title: appTitle),
+        initialRoute: '/',
+        routes: <String, WidgetBuilder> {
+          '/': (context) => const MyHomePage(title: appTitle),
+          '/1': (context) => const Page1(),
+          '/2': (context) => const Page2(),
+          '/3': (context) => const Page3(),
+          '/4': (context) => const Page4(),
+        }
+
       );
 }
 
@@ -36,33 +44,44 @@ class _HomePageState extends State<MyHomePage> {
     const Page4(),
   ];
 
+  final controller = PageController();
+
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: Text(MyApp.appTitle)),
-        body: pages[pageIndex],
+        appBar: AppBar(title: const Text(MyApp.appTitle)),
+        body: PageView(
+          controller: controller,
+          children: const [
+            Page1(),
+            Page2(),
+            Page3(),
+            Page4(),
+          ],
+        ),
         drawer: Drawer(
           child: ListView(
             padding: EdgeInsets.zero,
             children: [
               const DrawerHeader(
                 decoration: BoxDecoration(
-                  color: Colors.blue,
+                  color: const Color.fromARGB(255, 129, 154, 235),
                 ),
                 child: Text('Drawer Header'),
               ),
               ListTile(
-                title: const Text('Item 1'),
+                title: const Text('Page 1'),
                 onTap: () {
                   // Update the state of the app.
                   // ...
-                  Navigator.canPop(context);
+                  Navigator.pushNamed(context, '/1');
                 },
               ),
               ListTile(
-                title: const Text('Item 2'),
+                title: const Text('Page 2'),
                 onTap: () {
                   // Update the state of the app.
                   // ...
+                  Navigator.pushNamed(context, '/2');
                 },
               ),
             ],
@@ -82,11 +101,7 @@ class _HomePageState extends State<MyHomePage> {
             children: [
               IconButton(
                 enableFeedback: false,
-                onPressed: () {
-                  setState(() {
-                    pageIndex = 0;
-                  });
-                },
+                onPressed: () => setState(() => pages[0]),
                 icon: const Icon(
                   Icons.home_outlined,
                   color: Colors.white,
@@ -95,11 +110,7 @@ class _HomePageState extends State<MyHomePage> {
               ),
               IconButton(
                 enableFeedback: false,
-                onPressed: () {
-                  setState(() {
-                    pageIndex = 1;
-                  });
-                },
+                onPressed: () => setState(() => pages[1]),
                 icon: const Icon(
                   Icons.work_outline_outlined,
                   color: Colors.white,
@@ -108,11 +119,7 @@ class _HomePageState extends State<MyHomePage> {
               ),
               IconButton(
                 enableFeedback: false,
-                onPressed: () {
-                  setState(() {
-                    pageIndex = 2;
-                  });
-                },
+                onPressed: () => setState(() => pages[2]),
                 icon: const Icon(
                   Icons.widgets_outlined,
                   color: Colors.white,
@@ -121,11 +128,7 @@ class _HomePageState extends State<MyHomePage> {
               ),
               IconButton(
                 enableFeedback: false,
-                onPressed: () {
-                  setState(() {
-                    pageIndex = 3;
-                  });
-                },
+                onPressed: () => setState(() => pages[3]),
                 icon: const Icon(
                   Icons.person_outline,
                   color: Colors.white,
@@ -142,17 +145,23 @@ class Page1 extends StatelessWidget {
   const Page1({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Container(
-        color: const Color(0xffC4DFCB),
-        child: Center(
-          child: Text(
-            "Page Number 1",
-            style: TextStyle(
-              color: Colors.green[900],
-              fontSize: 45,
-              fontWeight: FontWeight.w500,
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: const Color(0xffC4DFCB),
+        body: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: Text(
+                "Page Number 1",
+                style: TextStyle(
+                  color: Colors.green.shade900,
+                  fontSize: 45,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
             ),
-          ),
+          ],
         ),
       );
 }
@@ -162,6 +171,7 @@ class Page2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Container(
+      child: Material(
         color: const Color(0xffC4DFCB),
         child: Center(
           child: Text(
@@ -171,9 +181,10 @@ class Page2 extends StatelessWidget {
               fontSize: 45,
               fontWeight: FontWeight.w500,
             ),
-          ),
+          )
         ),
-      );
+      ),
+  );
 }
 
 class Page3 extends StatelessWidget {
